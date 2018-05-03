c0594d7e8cb8869f3d1171a8b3b2d950e9561ba0
judas <judas@Judas-PC>
2018-05-03 14:45:02 +0800

    aaa

---
+++ src/main/java/com/bunfly/pojo/Menu.java
@@ -0,0 +1,73 @@
+package com.bunfly.pojo;
+
+public class Menu {
+
+	private int pid;
+	private String pname;
+	private int mid;
+	private int kid;
+	private int price;
+	private int hid;
+	private Merchant mname;
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public int getMid() {
+		return mid;
+	}
+	public void setMid(int mid) {
+		this.mid = mid;
+	}
+	public int getKid() {
+		return kid;
+	}
+	public void setKid(int kid) {
+		this.kid = kid;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getHid() {
+		return hid;
+	}
+	public void setHid(int hid) {
+		this.hid = hid;
+	}
+	public Merchant getMname() {
+		return mname;
+	}
+	public void setMname(Merchant mname) {
+		this.mname = mname;
+	}
+	public Menu(int pid, String pname, int mid, int kid, int price, int hid, Merchant mname) {
+		super();
+		this.pid = pid;
+		this.pname = pname;
+		this.mid = mid;
+		this.kid = kid;
+		this.price = price;
+		this.hid = hid;
+		this.mname = mname;
+	}
+	public Menu() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Menu [pid=" + pid + ", pname=" + pname + ", mid=" + mid + ", kid=" + kid + ", price=" + price + ", hid="
+				+ hid + ", mname=" + mname + "]";
+	}
+	
+}
---
+++ src/main/java/com/bunfly/pojo/Merchant.java
@@ -0,0 +1,59 @@
+package com.bunfly.pojo;
+
+import java.util.List;
+
+public class Merchant {
+
+	private int mid;
+	private String username;
+	private String password;
+	private int cid;
+	private List<Menu> menu;
+	public int getMid() {
+		return mid;
+	}
+	public void setMid(int mid) {
+		this.mid = mid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public List<Menu> getMenu() {
+		return menu;
+	}
+	public void setMenu(List<Menu> menu) {
+		this.menu = menu;
+	}
+	public Merchant(int mid, String username, String password, int cid, List<Menu> menu) {
+		super();
+		this.mid = mid;
+		this.username = username;
+		this.password = password;
+		this.cid = cid;
+		this.menu = menu;
+	}
+	public Merchant() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Merchant [mid=" + mid + ", username=" + username + ", password=" + password + ", cid=" + cid + ", menu="
+				+ menu + "]";
+	}
+	
+}
